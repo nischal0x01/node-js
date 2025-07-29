5d7349ca31a00147bfb0087b6c3774f9b25f9c15
Nischal Subedi <nischal1337@gmail.com>
2025-07-29 13:56:46 +0545

    node js again

--- chapter3/src/route/authRoutes.js
+++
@@ -1,8 +0,0 @@
-import express from 'express'
-import bcrypt from 'bcryptjs'
-import jwt from 'jsonwebtoken'
-import database from '../db.js'
-
-const router = express.Router()
-
-export default router
